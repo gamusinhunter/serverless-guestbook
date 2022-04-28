45380e6269fc94d7bbc0ee126311fc7ee6a46322
gamusinhunter <97673308+gamusinhunter@users.noreply.github.com>
2022-04-28 11:31:33 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fdb90f36.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
